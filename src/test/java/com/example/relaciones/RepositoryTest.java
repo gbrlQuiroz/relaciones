4492d20c691a4cc9beb60ec5c14d99adcdf339b9
gbrlQuiroz <orion_fire_qog@hotmail.com>
2022-02-02 18:04:14 -0600

    test completo del repository

--- src/test/java/com/example/relaciones/RepositoryTest.java
+++ src/test/java/com/example/relaciones/RepositoryTest.java
@@ -9,6 +9,11 @@ import com.example.relaciones.persistences.NotaRepository;
 import com.example.relaciones.persistences.PersonaRepository;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+
 // import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,13 +35,19 @@ public class RepositoryTest {
     @Autowired
     private NotaRepository nr;
 
+    @AfterEach
+    void tearDown() {
+        pr.deleteAll();
+        nr.deleteAll();
+    }
+
     // mvn clean ; mvn test -Dtest=RepositoryTest#database
     @Test
     // @Transactional(readOnly = true)
     public void database() throws Exception {
-        Persona persona1 = new Persona(null, "gabriel", "quiroz", "hombre", 45,null);
-        Persona persona2 = new Persona(null, "lulu", "quiroz", "mujer", 44,null);
-        Persona persona3 = new Persona(null, "elianid", "tolentino", "mujer", 40,null);
+        Persona persona1 = new Persona(null, "gabriel", "quiroz", "hombre", 45, null);
+        Persona persona2 = new Persona(null, "lulu", "quiroz", "mujer", 44, null);
+        Persona persona3 = new Persona(null, "elianid", "tolentino", "mujer", 40, null);
 
         // pr.save(new Persona(null, "gabriel", "quiroz", "hombre", 45));
         pr.save(persona1);
@@ -73,5 +84,34 @@ public class RepositoryTest {
         nr.findByPersonaId(1L).forEach(dato -> {
             System.out.println(dato.toString());
         });
+    }
+
+    // mvn clean ; mvn test -Dtest=RepositoryTest#itShouldCheckIfDataExist
+    @Test
+    public void itShouldCheckIfDataExist() throws Exception {
+        // given
+        Persona persona2 = new Persona(null, "gabriel", "quiroz", "hombre", 45, null);
+        pr.save(persona2);
+        nr.save(new Nota(null, "es un chingon", persona2));
+        nr.save(new Nota(null, "y ya le salio esta madre", persona2));
+
+        // when
+        Persona expected = pr.findById(2L);
+        long cuentaPersona = pr.count();
+        List<Persona> personas = pr.findByApPaterno("quiroz");
+        int sizePersonas = personas.size();
+
+        List<Nota> notas = nr.findByPersonaId(2L);
+        int sizeNotas = notas.size();
+
+
+        // then
+        assertThat(expected).isEqualTo(persona2);
+        assertThat(cuentaPersona).isEqualTo(2L);
+        assertThat(sizePersonas).isEqualTo(1);
+        assertThat(sizeNotas).isEqualTo(2);
+
+
+
     }
 }
